fad66db735ecbc7e845422d93acb5bc04e95f1af
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-24 17:45:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Write your code in this file!
 scuberGreetingForFeet(distance) {
+  if (distance < 400) {
 
+  } else if (distance < 2000) {
+
+  } else {
+    
+  }
 }
 
 ternaryCheckCity(destination) {
-  return destination === 'NYC' ? ""
+  return destination === 'NYC' ? "Okay, sounds good." : "No go."
 }
 
 switchOnCharmFromTip() {
